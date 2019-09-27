07ddfd32c296b6c12f398b5a88dec33716d1983a
Thomas Barregren <thomas@kntnt.se>
2019-09-27 16:15:15 +0200

    Added support for afragen/github-updater.

--- kntnt-editor-notes.php
+++ kntnt-editor-notes.php
@@ -4,9 +4,10 @@
  * @wordpress-plugin
  * Plugin Name:       Kntnt Editor Notes
  * Description:       Provides the shortcode [NOTE]…[/NOTE] where … can be anything visible only for editors.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
+ * GitHub Plugin URI: https://github.com/Kntnt/kntnt-editor-notes
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
